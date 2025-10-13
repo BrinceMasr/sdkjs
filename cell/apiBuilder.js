3b0d2e1a1695009e4d032ec9f33d6cd41319efc8
Angsar <ansar.aydarbek@onlyoffice.com>
2025-10-13 12:01:55 +0500

    [all] Fix bug 62721

--- cell/apiBuilder.js
+++ cell/apiBuilder.js
@@ -10959,75 +10959,75 @@
 	 * @param {SortOrientation} sOrientation - Specifies if the sort should be by row (default) or column.
 	 * @see office-js-api/Examples/{Editor}/ApiRange/Methods/SetSort.js
 	 */
-	ApiRange.prototype.SetSort = function (key1, sSortOrder1, key2, /*Type,*/ sSortOrder2, key3, sSortOrder3, sHeader, /*OrderCustom, MatchCase,*/ sOrientation/*, SortMethod, DataOption1, DataOption2, DataOption3*/) {
-		var ws = this.range.worksheet;
-		var sortSettings = new Asc.CSortProperties(ws);
-		var range = this.range.bbox;
+    ApiRange.prototype.SetSort = function (key1, sSortOrder1, key2, /*Type,*/ sSortOrder2, key3, sSortOrder3, sHeader, /*OrderCustom, MatchCase,*/ sOrientation/*, SortMethod, DataOption1, DataOption2, DataOption3*/) {
+        var ws = this.range.worksheet;
+        var sortSettings = new Asc.CSortProperties(ws);
+        var range = this.range.bbox;
 
-		var aMerged = ws.mergeManager.get(range);
-		if (aMerged.outer.length > 0 || (aMerged.inner.length > 0 && null == window['AscCommonExcel']._isSameSizeMerged(range, aMerged.inner, true))) {
-			return;
-		}
+        var aMerged = ws.mergeManager.get(range);
+        if (aMerged.outer.length > 0 || (aMerged.inner.length > 0 && null == window['AscCommonExcel']._isSameSizeMerged(range, aMerged.inner, true))) {
+            return;
+        }
 
-		sortSettings.hasHeaders = sHeader === "xlYes";
-		var columnSort = sortSettings.columnSort = sOrientation !== "xlSortRows";
+        sortSettings.hasHeaders = sHeader === "xlYes";
+        var columnSort = sortSettings.columnSort = sOrientation !== "xlSortRows";
 
-		var getSortLevel = function (_key, _order) {
-			var index = null;
-			if (_key instanceof ApiRange) {
-				index = columnSort ? _key.range.bbox.c1 - range.c1 : _key.range.bbox.r1 - range.r1;
-			} else if (typeof _key === "string") {
-				//named range
-				var _defName = ws.workbook.getDefinesNames(_key);
-				if (_defName) {
-					var defNameRef;
-					AscCommonExcel.executeInR1C1Mode(false, function () {
-						defNameRef = AscCommonExcel.getRangeByRef(_defName.ref, ws, true, true)
-					});
-					if (defNameRef && defNameRef[0] && defNameRef[0].worksheet) {
-						if (range.contains(defNameRef[0].bbox.c1, defNameRef[0].bbox.r1)) {
-							if (defNameRef[0].worksheet.Id === ws.Id) {
-								index = columnSort ? defNameRef[0].bbox.c1 - range.c1 : defNameRef[0].bbox.r1 - range.r1;
-							}
-						} else {
-							//error
-							return false;
-						}
-					}
-				}
-			}
+        var getSortLevel = function (_key, _order) {
+            var index = null;
+            if (_key instanceof ApiRange) {
+                index = columnSort ? _key.range.bbox.c1 - range.c1 : _key.range.bbox.r1 - range.r1;
+            } else if (typeof _key === "string") {
+                //named range
+                var _defName = ws.workbook.getDefinesNames(_key);
+                if (_defName) {
+                    var defNameRef;
+                    AscCommonExcel.executeInR1C1Mode(false, function () {
+                        defNameRef = AscCommonExcel.getRangeByRef(_defName.ref, ws, true, true)
+                    });
+                    if (defNameRef && defNameRef[0] && defNameRef[0].worksheet) {
+                        if (range.contains(defNameRef[0].bbox.c1, defNameRef[0].bbox.r1)) {
+                            if (defNameRef[0].worksheet.Id === ws.Id) {
+                                index = columnSort ? defNameRef[0].bbox.c1 - range.c1 : defNameRef[0].bbox.r1 - range.r1;
+                            }
+                        } else {
+                            //error
+                            return false;
+                        }
+                    }
+                }
+            }
 
-			if (null === index) {
-				return null;
-			}
+            if (null === index) {
+                return null;
+            }
 
-			var level = new Asc.CSortPropertiesLevel();
-			level.index = index;
-			level.descending = _order === "xlDescending" ? Asc.c_oAscSortOptions.Descending : Asc.c_oAscSortOptions.Ascending;
-			sortSettings.levels.push(level);
-		};
+            var level = new Asc.CSortPropertiesLevel();
+            level.index = index;
+            level.descending = _order === "xlDescending" ? Asc.c_oAscSortOptions.Descending : Asc.c_oAscSortOptions.Ascending;
+            sortSettings.levels.push(level);
+        };
 
-		sortSettings.levels = [];
-		if (key1 && false === getSortLevel(key1, sSortOrder1)) {
-			return;
-		}
-		if (key2 && false === getSortLevel(key2, sSortOrder2)) {
-			return;
-		}
-		if (key3 && false === getSortLevel(key3, sSortOrder3)) {
-			return;
-		}
+        sortSettings.levels = [];
+        if (key1 && false === getSortLevel(key1, sSortOrder1)) {
+            return;
+        }
+        if (key2 && false === getSortLevel(key2, sSortOrder2)) {
+            return;
+        }
+        if (key3 && false === getSortLevel(key3, sSortOrder3)) {
+            return;
+        }
 
-		var oWorksheet = Asc['editor'].wb.getWorksheet();
-		var tables = ws.autoFilters.getTablesIntersectionRange(range);
-		var obj;
-		if (tables && tables.length) {
-			obj = tables[0];
-		} else if (ws.AutoFilter && ws.AutoFilter.Ref && ws.AutoFilter.Ref.intersection(range)) {
-			obj = ws.AutoFilter;
-		}
-		ws.setCustomSort(sortSettings, obj, null, oWorksheet && oWorksheet.cellCommentator, range);
-	};
+        var oWorksheet = Asc['editor'].wb.getWorksheet();
+        var tables = ws.autoFilters.getTablesIntersectionRange(range);
+        var obj;
+        if (tables && tables.length) {
+            obj = tables[0];
+        } else if (ws.AutoFilter && ws.AutoFilter.Ref && ws.AutoFilter.Ref.intersection(range)) {
+            obj = ws.AutoFilter;
+        }
+        ws.setCustomSort(sortSettings, obj, null, oWorksheet && oWorksheet.cellCommentator, range);
+    };
 
 	/*Object.defineProperty(ApiRange.prototype, "Sort", {
 		set: function (obj) {
