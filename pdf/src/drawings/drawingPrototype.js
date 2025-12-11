4cb28cc5c0fab9de1090b3a5d34841d8b3932352
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2025-12-11 18:49:31 +0700

    Fix

--- pdf/src/drawings/drawingPrototype.js
+++ pdf/src/drawings/drawingPrototype.js
@@ -592,7 +592,7 @@
         let sFontName = oTextPr.GetFontFamily();
 
         let oFontFile;
-        if (sFontName.startsWith(AscFonts.getEmbeddedFontPrefix())) {
+        if (sFontName && sFontName.startsWith(AscFonts.getEmbeddedFontPrefix())) {
             let oFontInfo = AscFonts.g_font_infos_embed[AscFonts.g_map_font_index_embed[sFontName]];
             oFontFile = AscCommon.g_fontManager.LoadFont(AscCommon.g_font_loader.fontFiles[oFontInfo.indexR], oFontInfo.faceIndexR, AscFonts.MEASURE_FONTSIZE,
                 false,
