b06a75f24fff22bf538abed73f2d9b844c9097b3
Sergey Luzyanin <Sergey.Luzyanin@onlyoffice.com>
2026-05-21 18:48:20 +0300

    [pe] Refactor: use cSld for Slide, Master, Layout, Notes

--- common/apiBase.js
+++ common/apiBase.js
@@ -1963,7 +1963,6 @@
 		this.CoAuthoringApi.onSetIndexUser            = function(e)
 		{
 			AscCommon.g_oIdCounter.Set_UserId('' + e);
-			AscCommon.g_oDrawingIdAllocator.setUserIndex(e);
 		};
 		this.CoAuthoringApi.onFirstLoadChangesEnd     = function(openedAt)
 		{
--- word/Editor/Document.js
+++ word/Editor/Document.js
@@ -15669,7 +15669,6 @@ CDocument.prototype.StartNoHistoryMode = function()
 
 	this.History.TurnOff();
 	this.TableId.TurnOff();
-	AscCommon.g_oDrawingIdAllocator.TurnOff();
 	this.SetLocalTrackRevisions(false);
 
 	return oState;
@@ -15682,7 +15681,6 @@ CDocument.prototype.EndNoHistoryMode = function(oState)
 {
 	this.History.TurnOn();
 	this.TableId.TurnOn();
-	AscCommon.g_oDrawingIdAllocator.TurnOn();
 	this.SetLocalTrackRevisions(oState.LocalTrackRevisions);
 };
 /**
